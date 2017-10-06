71132277d6ffb57753941be4e3178862803f176d
Fuji_Toan <toan.nguyen@fujitechjsc.com>
2017-10-06 15:06:19 +0700

    Update jenkins file and fix auto migrate db

--- linux/scripts/auto-migrate-database-simple.sh
+++ linux/scripts/auto-migrate-database-simple.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SQL file format: YYYYMMDDHHII_XXXXX.sql
-#SQL path
+#SQL folder path
 cd "/usr/local/bin/myscript/sql/"
 ls *.sql > list
 #Check file log file
 if [ -f "log" ]
 then
-    value=`cat $wd"log"`
+    value=`cat log`
 else
     value=""
 fi
@@ -15,7 +15,7 @@ while read line; do
 if [ "$line" \> "$value" ]
 then
     #Run script from file command
-    echo "$line"
+    psql -U user -d database-name -a -f $line
 fi
 done < list
 tail -n 1 list > log
